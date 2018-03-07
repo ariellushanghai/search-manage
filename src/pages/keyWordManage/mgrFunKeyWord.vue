80214e97a961084c04ec5309efa198f79a64b799
LUHUAN <luhuan994@pingan.com.cn>
2018-03-07 17:57:43 +0800

    0307

--- src/pages/keyWordManage/mgrFunKeyWord.vue
+++ src/pages/keyWordManage/mgrFunKeyWord.vue
@@ -23,19 +23,39 @@
                             | 搜索
                         el-button(type='primary', @click="", icon='el-icon-error', size='mini')
                             | 重置
+                        el-button(type='primary', @click="", icon='el-icon-download', size='mini')
+                            | 导出
 
-            el-card.card(:body-style="{padding:'10px'}")
-                el-table.table(:data='tableData', :height='table_height', :stripe='true', :border='true')
+            .card-like
+                el-table.table(:data='tableData', :height='table_height', :stripe='true', :border='true', size='mini', tooltip-effect='light')
                     el-table-column(prop='id', label='条目ID')
                     el-table-column(prop='name', label='功能名称')
-                    el-table-column(prop='name', label='功能名称')
+                    el-table-column(prop='parent', label='父级功能标识')
+                    el-table-column(prop='keyWords', label='关键词', :show-overflow-tooltip='true', width='200px')
+                    el-table-column(prop='weightWord', label='权重关键词')
+                    el-table-column(prop='url', label='跳转链接', width='300px')
+                    el-table-column(prop='sonUrl', label='子跳转链接')
+                    el-table-column(prop='actionType', label='跳转链接类型')
+                    el-table-column(prop='appVersion', label='最低可兼容APP版本')
+                    el-table-column(prop='level', label='功能层级')
+                    el-table-column(prop='fatherId', label='父节点ID')
+                    el-table-column(prop='status', label='功能标识')
+                    el-table-column(prop='remark', label='说明')
+                    el-table-column(prop='updateBy', label='更新人')
+                    el-table-column(prop='updateDate', label='更新日期')
+                    el-table-column(prop='clientSystem', label='客户端操作系统')
+                    el-table-column(prop='androidLowVersion', label='最低可兼容Android版本')
+                    el-table-column(prop='iosLowVersion', label='最低可兼容IOS版本')
+                    el-table-column(prop='weight', label='权重')
+                    el-table-column(prop='appRemark', label='功能标识')
+                    el-table-column(prop='imgUrl', label='图标链接')
 
 </template>
 
 <script>
   // @flow
   import API from '@/service/api'
-  import {map, extend, assign, debounce, isEmpty} from 'lodash'
+  import {map, extend, assign, debounce, isEmpty, cloneDeep} from 'lodash'
   import format from 'date-fns/format'
   import ElCard from "element-ui/packages/card/src/main";
 
@@ -61,12 +81,13 @@
       }
     },
     computed: {
-      tableData: () => {
-        return this.fun_key_words;
+      tableData: function () {
+        console.log(cloneDeep(this.fun_key_words))
+        return cloneDeep(this.fun_key_words);
       }
     },
     mounted() {
-      // this.fetchData();
+      this.fetchData();
       this.table_height = this.resizeHandler();
       console.log(this.resizeHandler());
       window.onresize = debounce(() => {
@@ -78,7 +99,7 @@
     },
     methods: {
       resizeHandler(): number {
-        return document.querySelector('#router_view').getBoundingClientRect().height - (20 + 64 + 10 + 30);
+        return document.querySelector('#router_view').getBoundingClientRect().height - (20 + 64 + 10 + 30 + 20);
       },
       fetchData() {
         let loading = this.$loading({
@@ -88,7 +109,8 @@
           background: 'rgba(250,235,215,0.5)'
         });
         return API.getAllApp().then(res => {
-          this.fun_key_words = res;
+          console.log(`res.list: `, res.list);
+          this.fun_key_words = res.list;
           loading.close();
         }, err => {
           console.error(`err: `, err);
@@ -165,14 +187,14 @@
     .user-manager-main
         display flex
         flex-direction column
-        padding 10px
+        padding 5px
         width 100%
         height 100%
         position relative
         overflow hidden
 
     .card
-        margin-bottom 10px
+        margin-bottom 5px
         flex-shrink 0
 
     .card:last-of-type
@@ -181,7 +203,7 @@
         height calc(100% - 74px)
 
     .card.operations /deep/ .el-card__body
-        padding 10px
+        padding 5px
 
     .card.operations .el-form
         width 100%
@@ -189,11 +211,19 @@
 
     .card.operations .el-form-item
         width 80%
-        margin-bottom 10px
+        margin-bottom 5px
 
     .table
         padding 0
         width 100%
         font-size 12px
+
+    .card-like
+        padding 5px
+        border 1px solid #ebeef5
+        background-color #fff
+        box-shadow 0 2px 12px 0 rgba(0, 0, 0, .1)
+        color #303133
+        border-radius 4px
 
 </style>
